303a325c923e76abc6cd14be73f73c9256030ef1
Rashen <rashen598@gmail.com>
2023-11-12 10:05:08 +0530

    added pingServer method in Controller

--- src/main/java/edu/icet/controller/ServerController.java
+++ src/main/java/edu/icet/controller/ServerController.java
@@ -1,16 +1,20 @@
 package edu.icet.controller;
 
 
+import edu.icet.enumeration.Status;
 import edu.icet.model.Response;
+import edu.icet.model.Server;
 import edu.icet.service.ServerService;
 import edu.icet.service.ServerServiceImpl;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.IOException;
 import java.time.LocalDateTime;
 import java.util.Map;
 
@@ -29,6 +33,20 @@ public class ServerController {
                         .timeStamp(LocalDateTime.now())
                         .data(Map.of("servers",serverService.list(30)))
                         .message("Server Retreived")
+                        .status(HttpStatus.valueOf(OK))
+                        .statusCode(OK)
+                        .build()
+        );
+    }
+
+    @GetMapping("/ping/{ipAddress}")
+    public ResponseEntity<Response> pingServer(@PathVariable String ipAddress) throws IOException {
+        Server server = serverService.ping(ipAddress);
+        return ResponseEntity.ok(
+                Response.builder()
+                        .timeStamp(LocalDateTime.now())
+                        .data(Map.of("servers",serverService.list(30)))
+                        .message(server.getStatus() == Status.SERVER_UP ? "Ping Success" : "Ping Failed")
                         .status(HttpStatus.valueOf(OK))
                         .statusCode(OK)
                         .build()
