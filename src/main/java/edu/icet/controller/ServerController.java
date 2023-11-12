b1f55e01785013b18445c2ea9551e5bd339348bf
Rashen <rashen598@gmail.com>
2023-11-12 10:29:35 +0530

    fixed bug in add image method

--- src/main/java/edu/icet/controller/ServerController.java
+++ src/main/java/edu/icet/controller/ServerController.java
@@ -97,6 +97,6 @@ public class ServerController {
 
     @GetMapping(path = "/image/{fileName}", produces = IMAGE_PNG_VALUE)
     public byte[] getServerImage(@PathVariable("fileName") String fileName) throws IOException {
-        return Files.readAllBytes(Paths.get(System.getProperty("user.home") + "Downloads/images/" + fileName))
+        return Files.readAllBytes(Paths.get(System.getProperty("user.home") + "Downloads/images/" + fileName));
     }
 }
