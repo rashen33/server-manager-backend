c37b98214f814726be96202d57c9e883b0ffdc74
Rashen <rashen598@gmail.com>
2023-11-12 13:03:59 +0530

    Added some sample servers

--- src/main/java/edu/icet/ServerManagerApplication.java
+++ src/main/java/edu/icet/ServerManagerApplication.java
@@ -1,7 +1,13 @@
 package edu.icet;
 
+import edu.icet.model.Server;
+import edu.icet.repository.ServerRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import static edu.icet.enumeration.Status.SERVER_DOWN;
+import static edu.icet.enumeration.Status.SERVER_UP;
 
 @SpringBootApplication
 public class ServerManagerApplication {
@@ -9,4 +15,12 @@ public class ServerManagerApplication {
 		SpringApplication.run(ServerManagerApplication.class, args);
 	}
 
+	CommandLineRunner run(ServerRepository serverRepository){
+		return args -> {
+			serverRepository.save(new Server(null, "192.168.1.160", "Ubuntu Linux", "16GB", "Personal PC", "http://localhost:8080/server/image/server1.png" , SERVER_UP));
+			serverRepository.save(new Server(null, "192.168.1.58", "Fedora", "16GB", "Dell Tower", "http://localhost:8080/server/image/server2.png" , SERVER_DOWN));
+			serverRepository.save(new Server(null, "192.168.1.160", "Ubuntu Linux", "16GB", "Personal PC", "http://localhost:8080/server/image/server1.png" , SERVER_UP));
+			serverRepository.save(new Server(null, "192.168.1.160", "Ubuntu Linux", "16GB", "Personal PC", "http://localhost:8080/server/image/server1.png" , SERVER_UP));
+		};
+	}
 }
